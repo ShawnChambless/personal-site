cefc97dd03d97c9515dd5dd737084bbbda2cfc0d
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-08 15:58:31 -0600

    base layout done

---
+++ public/productionFiles/minifiedJS/app.js
@@ -0,0 +1,1 @@
+var app=angular.module("personalSite",["ngRoute"]);app.config(function(e){e.when("/home",{templateUrl:"productionFiles/html/home/homeTmpl.html",controller:"homeCtrl"}).otherwise("/home")});
---
+++ server.js
@@ -0,0 +1,15 @@
+/* jshint node: true */
+
+'use strict';
+var express     = require('express'),
+    app         = express(),
+    http        = require( 'http' ),
+    favicon     = require( 'serve-favicon' ),
+    httpServer  = http.createServer(app);
+
+
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(__dirname + '/public'));
+
+
+httpServer.listen(8081, function(){console.log('Server is listening on 8081');});
