f84659f09dc445377efed106fd52891812207cfd
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-08 21:20:06 -0600

    production files

--- public/productionFiles/minifiedJS/app.js
+++ public/productionFiles/minifiedJS/app.js
@@ -1,1 +1,1 @@
-var app=angular.module("personalSite",["ngRoute"]);app.config(["$routeProvider",function(e){e.when("/home",{templateUrl:"productionFiles/html/home/homeTmpl.html",controller:"homeCtrl"}).otherwise("/home")}]);
+var app=angular.module("personalSite",["ngRoute"]);app.config(["$routeProvider",function(e){e.when("/home",{templateUrl:"productionFiles/html/home/homeTmpl.html",controller:"homeCtrl"}).when("/about",{templateUrl:"productionFiles/html/about/aboutTmpl.html"}).otherwise("/home")}]);
