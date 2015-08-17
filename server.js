d49b0b02729ebddd65bd9d3af1f8f869a522500a
Shawn Chambless <shawn.chambless@gmail.com>
2015-08-16 22:54:31 -0600

    add compression

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var express     = require('express'),
     app         = express(),
     http        = require( 'http' ),
     favicon     = require( 'serve-favicon' ),
+    compress    = require('compression'),
     httpServer  = http.createServer(app);
 
-
+app.use(compress());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 
